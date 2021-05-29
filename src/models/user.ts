7eabb5aef91001ecb3d1118f712383ece798f548
Martin Vera <muse@Martins-Air.home>
2021-05-29 14:23:33 -0700

    modified user.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,36 +1,67 @@
-import {
-    Sequelize,
-    DataTypes
+import { 
+    UUIDV4, Model 
 } from 'sequelize';
 
-export interface UserAttributes {
-    name ? : string;
-    email ? : string;
-    role ? : string;
-
-}
-
-export interface UserInstance {
-    id: number;
-    createdAt: Date;
-    updatedAt: Date;
-
+interface UserAttributes {
+    id: string,
     name: string;
     email: string;
     role: string;
-
 }
 
-export = (sequelize: Sequelize, DataTypes: DataTypes) => {
-    var User = sequelize.define('User', {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        role: DataTypes.STRING
-    });
+// export interface UserInstance {
+//     id: number;
+//     createdAt: Date;
+//     updatedAt: Date;
 
-    User.associate = function(models) {
-        // associations can be defined here
-    };
+//     name: string;
+//     email: string;
+//     role: string;
 
-    return User;
+// }
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class User extends Model<UserAttributes> 
+  implements UserAttributes {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    id!: string;
+    name!: string;
+    email!: string;
+    role!: string;
+    static associate(models: any) {
+      // define association here
+      User.belongsToMany(models.Project, {
+        through: 'ProjectAssignments'
+      })
+    }
+  };
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }, 
+    role: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
 };
